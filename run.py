3a1b5a32163f8f06c1587bd73768902cb2b2364c
mihaicm93 <45673304+mihaicm93@users.noreply.github.com>
2024-07-09 07:42:03 +0200

    test

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+
+    if os.getenv('FLASK_ENV') == 'production':
+        from gunicorn.app.wsgiapp import run
+        run()
+    else:
+        app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
